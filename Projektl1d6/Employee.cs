273fbd0f7c7e29ed468181df9c3c8858a70b9274
Dariusz Zając <darekdmc@gmail.com>
2023-08-16 15:21:43 +0200

    First part of app structure which need to find employee with best result and print his score and personal data in the console.

---
+++ Projektl1d6/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektl1d6
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();  
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set;}
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number) 
+        { 
+        this.score.Add(number);
+        }
+
+    }
+}
---
+++ Projektl1d6/Program.cs
@@ -0,0 +1,26 @@
+﻿using Projektl1d6;
+
+Employee employee1 = new Employee("Jan", "Kowalski", 20);
+Employee employee2 = new Employee("Janusz", "Bizensu", 30);
+Employee employee3 = new Employee("Marek", "Kondrat", 70);
+
+employee1.AddScore(1);
+employee1.AddScore(5);
+employee1.AddScore(5);
+employee1.AddScore(5);
+employee1.AddScore(5);
+
+employee2.AddScore(2);
+employee2.AddScore(3);
+employee2.AddScore(3);
+employee2.AddScore(3);
+employee2.AddScore(2);
+
+employee3.AddScore(1);
+employee3.AddScore(1);
+employee3.AddScore(2);
+employee3.AddScore(2);
+employee3.AddScore(5);
+
+var result = employee1.Result;
+Console.WriteLine(result);
